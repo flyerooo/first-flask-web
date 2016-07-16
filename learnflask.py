d2cf4433da27b357c44e041a27957670a8396679
flyerooo <wxl.pure@gmail.com>
2016-07-17 02:17:36 +0800

    add load file

--- learnflask.py
+++ learnflask.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 from werkzeug.routing import BaseConverter
+from werkzeug.utils import secure_filename
+from os import path
+
 
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
@@ -32,7 +35,23 @@ def projects():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+    else:
+        request.args['username']
     return render_template('login.html', method=request.method)
+
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    if request.method=='POST':
+        f = request.files['file']
+        basepath = path.abspath(path.dirname(__file__))
+        upload_path = path.join(basepath,'static/uploads')
+        f.save(upload_path,secure_filename(f.filename))
+        return redirect(url_for('upload'))
+    return render_template('upload.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
