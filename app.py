2c0238a32c83e5706742277709eeed799d0b8d7b
flyerooo <wxl.pure@gmail.com>
2016-07-17 15:01:08 +0800

    添加app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Created by Jeff on 2016/7/17 14:54
+from flask import Flask, render_template
+from livereload import Server
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="<h1>Hello World</h1>")
+
+
+if __name__ == '__main__':
+    live_server = Server(app.wsgi_app)
+    live_server.watch('**/*.*')
+    live_server.serve(open_url=True)
--- learnflask.py
+++ learnflask.py
@@ -1,8 +1,7 @@
-from flask import Flask,render_template,request,redirect,url_for,make_response,abort
+from flask import Flask, render_template, request, redirect, url_for, make_response, abort
 from werkzeug.routing import BaseConverter
 from werkzeug.utils import secure_filename
 from flask.ext.script import Manager
-
 
 from os import path
 
@@ -10,66 +9,76 @@ from os import path
 class RegexConverter(BaseConverter):
     def __init__(self, url_map, *items):
         super(RegexConverter, self).__init__(url_map)
-        self.regex=items[0]
+        self.regex = items[0]
+
 
 app = Flask(__name__)
 app.url_map.converters['regex'] = RegexConverter
-manager= Manager(app)
+manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    response = make_response(render_template('index.html',title='Welcome'))
-    response.set_cookie('username','')
+    response = make_response(render_template('index.html', title='Welcome'))
+    response.set_cookie('username', '')
 
     return response
+
 
 @app.route('/services')
 def services():
     return 'Service'
 
+
 @app.route('/about')
 def about():
     return 'About'
 
+
 @app.route('/user/<regex("[a-z]{3}"):user_id>')
 def user(user_id):
     return 'User %s' % user_id
+
 
 @app.route('/projects/')
 @app.route('/our-works/')
 def projects():
     return 'The project page'
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method=='POST':
-        username=request.form['username']
-        password=request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
     else:
         request.args['username']
     return render_template('login.html', method=request.method)
 
-@app.route('/upload',methods=['GET','POST'])
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    if request.method=='POST':
+    if request.method == 'POST':
         f = request.files['file']
         basepath = path.abspath(path.dirname(__file__))
-        upload_path = path.join(basepath,'static/uploads')
-        f.save(upload_path,secure_filename(f.filename))
+        upload_path = path.join(basepath, 'static/uploads')
+        f.save(upload_path, secure_filename(f.filename))
         return redirect(url_for('upload'))
     return render_template('upload.html')
 
+
 @app.errorhandler(404)
 def page_not_found(error):
-    return  render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 @manager.command
 def dev():
     from livereload import Server
-    liver_server= Server(app.wsgi_app)
+    liver_server = Server(app.wsgi_app)
     liver_server.watch('static/*.*')
     liver_server.serve(open_url=True)
+
 
 if __name__ == '__main__':
     manager.run()
