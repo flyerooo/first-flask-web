8a6b016a2aa9ac43fd9b85ef6b84f5a1b59d6a26
flyerooo <wxl.pure@gmail.com>
2016-07-17 15:33:02 +0800

    增加markdown方式显示

--- app.py
+++ app.py
@@ -9,8 +9,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index1.html', title="<h1>Hello World</h1>")
+    return render_template('index1.html', title="<h1>Hello World</h1>", body="## Header2")
 
+@app.template_filter('md')
+def markdown_to_html(txt):
+    from markdown import markdown
+    return markdown(txt)
+
+
+def read_md(filename):
+    with open(filename) as md_file:
+        content = reduce(lambda x,y:x+y,md_file.readlines())
+    return content.decode('utf-8')
+
+@app.context_processor
+def inject_methods():
+    return dict(read_md=read_md)
 
 if __name__ == '__main__':
     live_server = Server(app.wsgi_app)
